00ae75dbab1a48e648cf296ad8952de1efa262ea
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-06 10:01:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,38 +21,6 @@ class PigLatinizer
       end 
     end
     
-    
-   
-    
-
-
-def consonant
-  if self.match(/[aAeEiIoOuU]/)
-    return true
-  end
-end
-    
-    words.each do |w|
-      if w.consonant
-        w = w.split("").rotate(1).join("")
-      end
-      w
-    end 
-    
-    
-    
-    
-    
-  phr
-    if /^[^aeiou\W]/i.match(words)
-      return false 
-    else
-      return true
-    end
-    
-    
-    
-    
     words.join(" ")
   end 
 end
@@ -67,26 +35,26 @@ end
 
 #   private
 
-#   def consonant?(char)
-#     !char.match(/[aAeEiIoOuU]/)
-#   end
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
 
-#   def piglatinize_word(word)
-#     # word starts with vowel
-#     if !consonant?(word[0])
-#       word = word + "w"
-#     # word starts with 3 consonants
-#     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-#       word = word.slice(3..-1) + word.slice(0,3)
-#     # word starts with 2 consonants
-#     elsif consonant?(word[0]) && consonant?(word[1])
-#       word = word.slice(2..-1) + word.slice(0,2)
-#     # word starts with 1 consonant
-#     else
-#       word = word.slice(1..-1) + word.slice(0)
-#     end
-#     word << "ay"
-#   end
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
 
 #   def piglatinize_sentence(sentence)
 #     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
